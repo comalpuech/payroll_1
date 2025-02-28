dd4647c33c7f500b11f99dbc004ef0acd26c1be2
Come-Alexis Puech <n12311596@qut.edu.au>
2025-02-28 19:45:55 +1000

    start

--- src/main/java/org/payroll/hr/Employee.java
+++ src/main/java/org/payroll/hr/Employee.java
@@ -3,6 +3,16 @@ package org.payroll.hr;
 
 public abstract class Employee  {
 
+    private String name;
+    private double payRate;
+    private int EMPLOYEE_ID;
+    private final int nextID = 1000;
+    private final double STARTING_PAY_RATE =7.75;
 
+    public Employee(String name){
+        this.name=name;
+        this.payRate=0;
+
+    }
 
 }
